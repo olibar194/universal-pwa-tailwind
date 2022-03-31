67fd7873b143ac8aac580487aa44685272e519da
Juan Manuel Ibarra <olibar194@gmail.com>
2022-03-31 03:05:58 -0300

    metatags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'universal-pwa-tailwind';
+export class AppComponent implements OnInit {
+  constructor(private metaTagService: Meta) {}
+  ngOnInit() {
+    this.metaTagService.addTags([
+      {
+        name: 'keywords',
+        content: 'Angular SEO Integration, Tailwind, PWA, Angular Universal',
+      },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'Juan Olibar' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'date', content: '2022-3-31', scheme: 'YYYY-MM-DD' },
+      { charset: 'UTF-8' },
+    ]);
+  }
 }
